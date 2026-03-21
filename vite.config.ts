02e6368b856fe8b4c8b7dddd4c335ac335fe46d2
ASV-ship-it <svabyshev@gmail.com>
2026-03-21 09:09:36 +0300

    Update vite.config.ts
    fix: set base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({base: "/arhiput-app/",
+  plugins: [react()],}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
